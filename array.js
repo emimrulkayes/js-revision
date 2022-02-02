1514cdec866b5468242e8d23fec5afc4020c4271
MD. IMRUL KAYES <66476720+emimrulkayes@users.noreply.github.com>
2022-02-03 05:43:17 +0600

    first commit

---
+++ array.js
@@ -0,0 +1,10 @@
+// oddNumber in Array 
+var oddNumbers = [1, 3, 5, 7, 9, 11];
+
+// evenNumber in Array 
+var evenNumbers = [2, 4, 6, 8, 10, 12];
+
+var names = ['ragim', 'rahim', 'korim', 'jahum', ' kabul', ]
+
+var numberCount = names.length;
+console.log(numberCount);
---
+++ variable.js
@@ -0,0 +1,10 @@
+// keyword, variable name, assign, value, semiclon. 
+
+// Number type Variable 
+var ricePrice = 231;
+
+// string variable
+var userName = 'rahim';
+
+// Array variable
+var ages = [32, 23, 43, 54, 12];
