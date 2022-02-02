7a3c04f4b601055030585b41f767f6de3ab60ecc
MD. IMRUL KAYES <66476720+emimrulkayes@users.noreply.github.com>
2022-02-03 05:57:17 +0600

    array

--- array.js
+++ array.js
@@ -4,7 +4,15 @@ var oddNumbers = [1, 3, 5, 7, 9, 11];
 // evenNumber in Array 
 var evenNumbers = [2, 4, 6, 8, 10, 12];
 
+// name Array 
 var names = ['ragim', 'rahim', 'korim', 'jahum', ' kabul', ]
 
+// Array count 
 var numberCount = names.length;
-console.log(numberCount);
+// console.log(numberCount);
+
+// indexOf 
+var numbers = [43, 54, 66, 32, 65, 87]; //( indexNumber start will 0 )
+var index = numbers.indexOf(66); // check array position
+numbers[2] = 54; // change Array element
+console.log(numbers);
