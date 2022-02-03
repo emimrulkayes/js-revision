8516a79e398ca8a247f746f8a01f1e71e1b26583
MD. IMRUL KAYES <66476720+emimrulkayes@users.noreply.github.com>
2022-02-03 06:16:22 +0600

    push and pop

---
+++ push-pop.js
@@ -0,0 +1,21 @@
+//// Add last element in the array
+var lastBench = ['kalam', 'balam', 'salam', 'hakim'];
+lastBench.push('jalam');
+lastBench.push('jajal');
+console.log(lastBench);
+
+//// Remove last Element in the Array 
+var friendsAge = [54, 65, 342, 64, 324, 54];
+friendsAge.pop();
+console.log(friendsAge);
+
+//// Add First element in the array
+var firstBench = ['kalam', 'balam', 'salam', 'hakim'];
+firstBench.unshift('jalam');
+firstBench.unshift('jajal');
+console.log(firstBench);
+
+//// Remove first Element in the Array 
+var numberAge = [54, 65, 342, 64, 324, 54];
+numberAge.shift();
+console.log(numberAge);
