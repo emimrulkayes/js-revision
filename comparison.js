4d3a8f08c9aa964eb7d23255a93a53f90277f7da
MD. IMRUL KAYES <66476720+emimrulkayes@users.noreply.github.com>
2022-02-03 06:33:29 +0600

    comparison

---
+++ comparison.js
@@ -0,0 +1,11 @@
+//// To compare from big to small
+console.log(5 > 9);
+
+//// To compare from small to big
+console.log(5 < 9);
+
+////To compare difference between
+console.log(5 == 9);
+
+//// To compare difference between
+console.log(5 != 9);
