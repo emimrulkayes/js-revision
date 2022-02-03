fb51bf212ef78204d9a2a1c9dd3b7e96f1388f97
MD. IMRUL KAYES <66476720+emimrulkayes@users.noreply.github.com>
2022-02-03 10:44:40 +0600

    added while loop

---
+++ nested-condition.js
@@ -0,0 +1,14 @@
+var butterPrice = 55;
+var toastPrice = 15;
+var teaPrice = 10;
+var myBudget = 12;
+
+if (butterPrice < myBudget) {
+    console.log('chalo amra butter diye dhud cha khabo');
+} else if (toastPrice < myBudget) {
+    console.log('Amra toast diye cha khabo');
+} else if (teaPrice < myBudget) {
+    console.log('Chalo Gorur Dhud diye cha khabo');
+} else {
+    console.log('amra shudu rong cha khabo');
+}
---
+++ while-loop.js
@@ -0,0 +1,27 @@
+/* While Loop  */
+var moneyGiven = 0; // loop variable
+while (moneyGiven < 10) {
+    console.log('Please give me my money !!');
+    moneyGiven++;
+}
+
+
+var num = 1; // loop variable
+while (num <= 10) {
+    // console.log(num);
+    num++;
+}
+
+/* Odd Number */
+var num = 1; // loop variable
+while (num <= 20) {
+    console.log(num);
+    num = num + 2;
+}
+
+/* even Number */
+var number = 0; // loop variable
+while (number <= 20) {
+    // console.log(number);
+    number = number + 2;
+}
