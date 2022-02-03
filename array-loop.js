4e1fac05969674afdd3796012c7e596c36e50f39
MD. IMRUL KAYES <66476720+emimrulkayes@users.noreply.github.com>
2022-02-03 11:43:26 +0600

    added array loop

---
+++ array-loop.js
@@ -0,0 +1,13 @@
+/* Array for Loop */
+var numbers = [43, 45, 76, 67, 89, 99, 34, 42];
+for (var i = 0; i < numbers.length; i++) {
+    var num = numbers[i];
+    console.log(num);
+}
+
+/* Example two  */
+const products = ['Aloo', 'Onion', 'Appel', 'Komola', 'Canacur', 'Busikuit'];
+for (let i = 0; i < products.length; i++) {
+    let product = products[i];
+    console.log(product);
+}
